e521d882786c43cf13fa0105ed4d9f31a4e237fd
Pixaxe3145 <88450692+Pixaxe3145@users.noreply.github.com>
2021-08-11 02:26:23 +0900

    Add files via upload

---
+++ data_mod.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import os
+
+os.chdir("F:\project\오션_2021")
+
+fmap=pd.read_excel("final_map.xlsx")
+fcur=pd.read_excel("final_current.xlsx")
+
+
---
+++ shortest_route.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 11 02:22:59 2021
+
+@author: kjw62
+"""
+
